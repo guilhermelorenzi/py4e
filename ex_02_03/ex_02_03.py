274b5d9feed285b5a91994cf316769541e0ea2e2
guilhermelorenzi <guilorenzi@gmail.com>
2021-10-03 17:23:13 -0300

    inicio repositorio

---
+++ ex_02_03/ex_02_03.py
@@ -0,0 +1,4 @@
+xh = input("Enter Hours: ")
+xr = input("Enter Rate: ")
+xp = round(float(xh) * float(xr),2)
+print("Pay:",xp)
---
+++ teste.py
@@ -0,0 +1,20 @@
+# def greet(lang):
+#     if lang == 'en':
+#         return 'Hola'
+#     elif lang == 'es':
+#         return 'Bonjour'
+#     elif lang == 'fr':
+#         return 'Hello'
+#
+# print(greet('en'),'Glenn')
+# print(greet('es'),'Santi')
+# print(greet('fr'),'Michael')
+
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
